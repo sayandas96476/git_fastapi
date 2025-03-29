02cf6e185bff29cba14efd0a7d7d90a591d9428f
sayandas96476 <sayandastaba@gmail.com>
2025-03-30 01:59:54 +0530

    sit dev done 1:59

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -3,7 +3,7 @@ from src.main import app
 
 client = TestClient(app)
 
-def test_add():
+def test_case1():
     payload = {"num1": 21, "num2": 10}
     
     response = client.post("/add/", json=payload)  # ✅ POST request
@@ -28,3 +28,11 @@ def test_add():
     response = client.post("/divide/", json=payload)  # ✅ POST request
     assert response.status_code == 200
     assert response.json() == {"result": 2.1 }
+
+
+def test_case2():
+    payload = {"num1": 4, "num2": 1}
+    
+    response = client.post("/add/", json=payload)  # ✅ POST request
+    assert response.status_code == 200
+    assert response.json() == {"result": 5 }

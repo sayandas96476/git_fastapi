c7d6100e9d27b347c4455315974adc54619695cb
sayandas96476 <sayandastaba@gmail.com>
2025-03-30 00:42:03 +0530

    Your commit message

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -3,9 +3,25 @@ from src.main import app
 
 client = TestClient(app)
 
-def test_create_item():
-    payload = {"num1": 2, "num2": 3}
+def test_add():
+    payload = {"num1": 21, "num2": 10}
     
     response = client.post("/add/", json=payload)  # ✅ POST request
     assert response.status_code == 200
-    assert response.json() == {"result": 5 }
+    assert response.json() == {"result": 31 }
+
+
+
+    response = client.post("/subtract/", json=payload)  # ✅ POST request
+    assert response.status_code == 200
+    assert response.json() == {"result": 11 }
+
+
+    response = client.post("/multiply/", json=payload)  # ✅ POST request
+    assert response.status_code == 200
+    assert response.json() == {"result": 210 }
+
+    response = client.post("/divide/", json=payload)  # ✅ POST request
+    assert response.status_code == 200
+    assert response.json() == {"result": 2.1 }
+
